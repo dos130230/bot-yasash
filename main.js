d2edbeb279c07d3ab046c724147daae5f0bbe249
Dostonbek O'ktamov <88940496+dos130230@users.noreply.github.com>
2022-03-01 14:15:34 +0500

    Update main.js

--- main.js
+++ main.js
@@ -23,9 +23,3 @@ form.onsubmit = (event) => {
         errorMessage.textContent = error.message
     }
 }
-
-// https://img-13.stickers.cloud/packs/ccabdb29-2070-4d15-90ca-33654c5a02ed/webp/7d6559e4-5f43-4253-8a21-4877000c10e0.webp //germanya
-// https://img-13.stickers.cloud/packs/ccabdb29-2070-4d15-90ca-33654c5a02ed/webp/edef8168-5413-4d69-9507-062326a5bdfa.webp // russia
-// https://img-13.stickers.cloud/packs/ccabdb29-2070-4d15-90ca-33654c5a02ed/webp/a472105b-3e6a-4de9-b5d9-b71061fe68f4.webp // china
-// https://img-13.stickers.cloud/packs/ccabdb29-2070-4d15-90ca-33654c5a02ed/webp/93f726d3-a6ff-4e1f-9b9a-1f8a7252fe51.webp // amerika
-// https://img-13.stickers.cloud/packs/ccabdb29-2070-4d15-90ca-33654c5a02ed/webp/e2784adf-90ea-47a6-839a-5c24970517d3.webp // fransya
